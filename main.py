11ba0beb969269998c3a93b4397c644a31b5b3f7
JujaKen <81864134+jujaken@users.noreply.github.com>
2024-05-20 17:22:50 +0300

    change click logic

--- main.py
+++ main.py
@@ -17,22 +17,32 @@ while True:
     fh, fw, _ = frame.shape
     if lm:
         landmarks = lm[0].landmark
-        for id, landmark in enumerate(landmarks[474:478]):
+        right = landmarks[474:478]
+        for id, landmark in enumerate(right):
             x: int = int(landmark.x * fw)
             y: int = int(landmark.y * fh)
-            cv2.circle(frame, (x, y), 3, (0, 255, 0))
             if id == 1:
+                # DOTO: create trace 
                 sx = int(2 * (landmark.x * sw))
                 sy = int(2 * (landmark.y * sh))
+                print(sx, sy)
                 pyautogui.moveTo(sx, sy)
+                cv2.circle(frame, (x, y), 3, (0, 255, 0))
+            else:
+                cv2.circle(frame, (x, y), 3, (255, 255, 0))
+            
         left = [landmarks[145], landmarks[159]]
         for landmark in left:
             x = int(landmark.x * fw)
             y = int(landmark.y * fh)
             cv2.circle(frame, (x, y), 3, (0, 255, 255))
         # print(left[0].y - left[1].y)
-        if left[0].y - left[1].y < 0.007:
-            pyautogui.click()
+        leftEyeIsClose = left[0].y - left[1].y < 0.007
+        rightEyeIsOpen = right[0].y - right[1].y > 0.007
+        if leftEyeIsClose and rightEyeIsOpen:
+            pyautogui.mouseDown()
             pyautogui.sleep(0.5)
+        else:
+            pyautogui.mouseUp()
     cv2.imshow('Eye Cursor', frame)
     cv2.waitKey(1)
