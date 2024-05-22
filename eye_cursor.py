5926acae1ca2546269febd0a78c49ecb51f30da2
JujaKen <81864134+jujaken@users.noreply.github.com>
2024-05-22 13:09:50 +0300

    super mega refactoring

---
+++ eye_cursor.py
@@ -0,0 +1,105 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+
+# init eye cursor modules
+
+from eye_cursor import *
+
+# classes
+
+class EyeLandmark:
+
+    def __init__(self, name, id, color):
+        self.name = name
+        self.id = id
+        self.color = color
+
+    def isCorrent(self):
+        return self.id > 0
+
+    def landmark(self, landmarks):
+        return landmarks[self.id]
+
+    def pos(self, landmarks, fw, fh):
+        landmark = self.landmark(landmarks)
+        return int(landmark.x * fw), int(landmark.y * fh)
+
+class Eye:
+
+    def __init__(self, up: int, down: int, left: int, right: int, minDist, color):
+        self.up = EyeLandmark('up', up, color)
+        self.down = EyeLandmark('down', down, color)
+        self.left = EyeLandmark('left', left, color)
+        self.right = EyeLandmark('right', right, color)
+        self.minDist = minDist
+        self.color = color
+
+    def eyeLandmarks(self) -> list[EyeLandmark] :
+        tbl = []
+        if self.up.isCorrent(): tbl.append(self.up)
+        if self.down.isCorrent(): tbl.append(self.down)
+        if self.left.isCorrent(): tbl.append(self.left)
+        if self.right.isCorrent(): tbl.append(self.right)
+        return tbl
+    
+    def center(self, landmarks):
+        return (0, 0)
+    
+    def isOpen(self, landmarks) -> bool :
+        return True
+
+class EyeCursorApp:
+
+    def __init__(self, primaryEye, secondaryEye, circleRadius, defaultColor = (0, 0, 255)):
+        self.primaryEye, self.secondaryEye = primaryEye, secondaryEye
+        self.fm = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+        self.sh, self.sw = pyautogui.size()
+        self.cam = cv2.VideoCapture(0)
+        self.circleRadius = circleRadius
+        self.defaultColor = defaultColor
+
+    # private: 
+
+    def _updateFrame(self):
+        _, self.frame = self.cam.read()
+        self.frame = cv2.flip(self.frame, 1)
+        self.fh, self.fw, _ = self.frame.shape
+        self.rgb_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
+
+    def _updateLandmarks(self):
+        lm = self.fm.process(self.rgb_frame).multi_face_landmarks
+        self.lm = lm and lm[0].landmark
+
+    def _drawAllLandmarks(self):
+        for landmark in self.lm:
+            x = int(landmark.x * self.fw)
+            y = int(landmark.y * self.fh)
+            cv2.circle(self.frame, (x, y), self.circleRadius, self.defaultColor)
+
+    def _drawEyeLandmarks(self):
+        for eye in [self.primaryEye, self.secondaryEye]:
+            for eyeLandmark in eye.eyeLandmarks():
+                x, y = eyeLandmark.pos(self.lm, self.fw, self.fh)
+                cv2.circle(self.frame, (x, y), self.circleRadius, eyeLandmark.color)
+
+    def _clickControl(eye : Eye, name : str):
+        (eye.isOpen() and pyautogui.mouseDown or pyautogui.mouseUp)(button=name)
+
+    # public:
+
+    def setput(self):
+        pyautogui.FAILSAFE = True
+
+    def tick(self, all = False):
+        self._updateFrame()
+        self._updateLandmarks()
+        if self.lm: (all and self._drawAllLandmarks or self._drawEyeLandmarks)()
+        cv2.imshow('Eye Cursor', self.frame)
+        cv2.waitKey(1)
+
+    def moveCursor(self):
+        pass
+    
+    def useControls(self):
+        pass
---
+++ eye_cursor_config.py
@@ -0,0 +1,13 @@
+
+from eye_cursor import Eye
+
+red = (255, 0, 0)
+blue = (0, 255, 0)
+
+leftEye = Eye(145, 159, -1, -1, .007, red)
+rightEye = Eye(474, 475, 476, 477, .007, blue)
+
+primaryEye = leftEye
+secondaryEye = rightEye
+
+circleRadius = 3
