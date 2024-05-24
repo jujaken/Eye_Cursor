83dd28d8268d716805fd93b336760e99457eee02
JujaKen <81864134+jujaken@users.noreply.github.com>
2024-05-24 11:54:17 +0300

    controller

--- eye_cursor.py
+++ eye_cursor.py
@@ -7,6 +7,11 @@ import pyautogui
 
 from eye_cursor import *
 
+_FACE_W_LANDMARK_LEFT = 234
+_FACE_W_LANDMARK_RIGHT = 454
+_FACE_H_LANDMARK_UP = 10
+_FACE_H_LANDMARK_DOWN = 152
+
 # classes
 
 class EyeLandmark:
@@ -30,21 +35,53 @@ class Eye:
 
     lms = []
 
+    # up - upper eyelid
+    # down, left, right - parts of the pupil
+    def __init__(self, lidUp : int, lidDown : int, center : int, up: int, down: int, left: int, right: int, eyeCloseDiv, color):
+        self.lidUp = lidUp
+        self._addLandMark('lidUp', lidUp, color)
+        self.lidDown = lidDown
+        self._addLandMark('lidDown', lidDown, color)
+        self.center = center
+        self._addLandMark('center', center, color)
+        self.up = up
+        self._addLandMark('up', up, color)
+        self.down = down
+        self._addLandMark('down', down, color)
+        self.left = left
+        self._addLandMark('left', left, color)
+        self.right = right
+        self._addLandMark('right', right, color)
+        self.eyeCloseDiv = eyeCloseDiv
+        self.color = color
+
+    # private
+
     def _addLandMark(self, name, id, color):
         self.lms.append(EyeLandmark(name, id, color))
 
-    # up - upper eyelid
-    # down, left, right - parts of the pupil
-    def __init__(self, lidUp : int, lidDown : int, center : int, up: int, down: int, left: int, right: int, minDist, color):
-        self._addLandMark('lidUp', lidUp, color)
-        self._addLandMark('lidDown', lidDown, color)
-        self._addLandMark('center', center, color)
-        self._addLandMark('up', up, color)
-        self._addLandMark('down', down, color)
-        self._addLandMark('left', left, color)
-        self._addLandMark('right', right, color)
-        self.minDist = minDist
-        self.color = color
+    def _faceLeft(self, lm): return lm[_FACE_W_LANDMARK_LEFT]
+    
+    def _faceRight(self, lm): return lm[_FACE_W_LANDMARK_RIGHT]
+    
+    def _faceUp(self, lm): return lm[_FACE_H_LANDMARK_UP]
+    
+    def _faceDown(self, lm): return lm[_FACE_H_LANDMARK_DOWN]
+
+    def _faceW(self, lm):
+        vec1 = self._faceLeft(lm)
+        vec2 = self._faceRight(lm)
+        return vec1, vec2
+    
+    def _faceH(self, lm):
+        vec1 = self._faceUp(lm)
+        vec2 = self._faceDown(lm)
+        return vec1, vec2
+
+    def _dist(self, v1, v2):
+        return (v1.x - v2.x) ** 2 + (v1.y - v2.y) ** 2 + (v1.z - v2.z) ** 2
+
+    # public
 
     def eyeLandmarks(self) -> list[EyeLandmark] :
         tbl = []
@@ -52,7 +89,9 @@ class Eye:
         return tbl
     
     def isOpen(self, landmarks) -> bool :
-        return True
+        distLidSqrl = self._dist(landmarks[self.lidUp], landmarks[self.lidDown])
+        distPupilSqrl = self._dist(landmarks[self.up], landmarks[self.down])
+        return distPupilSqrl / self.eyeCloseDiv > distLidSqrl
 
 class EyeCursorApp:
 
@@ -72,11 +111,11 @@ class EyeCursorApp:
         self.fh, self.fw, _ = self.frame.shape
         self.rgb_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
 
-    def _calcW(self):
-        return self.fw * self.frameScale
+    def _calcW(self, x = 1):
+        return self.fw * self.frameScale * x
     
-    def _calcH(self):
-        return self.fh * self.frameScale
+    def _calcH(self, y = 1):
+        return self.fh * self.frameScale * y
 
     def _drawAllLandmarks(self):
         for id, landmark in enumerate(self.lm):
@@ -85,8 +124,15 @@ class EyeCursorApp:
             # cv2.circle(self.frame, (x, y), self.circleRadius, self.defaultColor)
             cv2.putText(self.frame, str(id), (x, y), 3, 0.5, self.defaultColor)
 
+    def _drawFace(self, func):
+        vec1, vec2 = func(self.lm)
+        cv2.circle(self.frame, (int(self._calcW(vec1.x)), int(self._calcH(vec1.y))), 5, self.defaultColor)
+        cv2.circle(self.frame, (int(self._calcW(vec2.x)), int(self._calcH(vec2.y))), 5, self.defaultColor)
+
     def _drawEyeLandmarks(self):
         for eye in [self.primaryEye, self.secondaryEye]:
+            self._drawFace(eye._faceW)
+            self._drawFace(eye._faceH)
             for eyeLandmark in eye.eyeLandmarks():
                 if not eyeLandmark.isCorrect(): continue
                 x, y = eyeLandmark.pos(self.lm, self._calcW(), self._calcH())
@@ -106,7 +152,9 @@ class EyeCursorApp:
         pass
     
     def useControls(self):
-        pass
+        for eye in [self.primaryEye, self.secondaryEye]:
+            if not eye.isOpen(self.lm): continue
+            cv2.circle(self.frame, (25, 25), 5, eye.color)
 
     def tick(self):
         self.updateLandmarks()
--- eye_cursor_config.py
+++ eye_cursor_config.py
@@ -4,8 +4,8 @@ from eye_cursor import Eye
 green = (255, 0, 0)
 blue = (0, 255, 0)
 
-rightEye = Eye(386, 374, 473, 475, 477, 476, 474, .007, blue)
-leftEye = Eye(159, 145, 468, 470, 472, 471, 469, .007, green)
+rightEye = Eye(386, 374, 473, 475, 477, 476, 474, 5, blue)
+leftEye = Eye(159, 145, 468, 470, 472, 471, 469, 5, green)
 
 primaryEye = leftEye
 secondaryEye = rightEye
